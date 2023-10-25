f55f04143d1a884eccb95fd673db446c994e013c
czg19970914 <chengzhigui97@163.com>
2023-10-25 23:55:19 +0800

    自定义TextView

--- app/src/main/java/com/example/customview/views/CoolTextView.kt
+++ app/src/main/java/com/example/customview/views/CoolTextView.kt
@@ -1,4 +1,13 @@
 package com.example.customview.views
 
-class CoolTextView {
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class CoolTextView: AppCompatTextView {
+    constructor(context: Context): super(context)
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+
+
 }
